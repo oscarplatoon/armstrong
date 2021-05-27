0ec4a6671e22ee7453de85168d188c6b5fc899c0
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-05-27 13:44:30 -0500

    Tianlin, Vince, and Shawn JS solution

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,5 +1,34 @@
 // How can you make this more scalable and reusable later?
 
-exports.findArmstrongNumbers = function() {
+exports.findArmstrongNumbers = function(num) {
+  // step 1: loop over numbers from 0 to 999
+  // step 2 : split number into digits
+  // step 3: cube digits and sum
+  // step 4: compare square of sums to number
+
+  let armstrongArray = [];
+
+  for (let i = 0; i < num.length; i++) {
+    let numString = i.toString();
+    numString = numString.split("")
+    let numArray = []
+
+    for (let x in numString) {
+      numArray[x] = parseInt(numString[x])
+    }
+
+    let sumOfDigits = 0;
+
+    for (let x in numArray) {
+      sumOfDigits += numArray[x] ** numArray.length;
+    }
+
+    if (i === sumOfDigits) {
+      armstrongArray.push(i);
+    }
+}
+
+
+  return armstrongArray;
 
 };
