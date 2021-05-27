604a478b4e9293f77be0ac6247dab8dbdb92513c
awhitford10 <awhitford@Andrews-Mac-mini.local>
2021-05-27 15:02:45 -0400

    Andrew and James_armstrong_python

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,16 @@
 # How can you make this more scalable and reusable later?
 def find_armstrong_numbers(numbers):
+    answers =[]
+    for number in numbers:
+        hold_number=number
+        sum = 0
+        digits = len(str(hold_number))
+        for x in range(digits,0,-1):
+            value = hold_number // (10 ** (x-1))
+            hold_number -= ((10 ** (x-1)) * value)
+            sum +=  (value ** digits)
+        if(sum == number):
+            answers.append(number) 
+
+    return(answers)
+
