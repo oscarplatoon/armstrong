c5ac0e1cc2489954194aa2b3880353ff210f83c0
AlexanderS68 <gruntmedic68@gmail.com>
2021-05-27 17:42:55 -0400

    SarahTim and Alexander Armstrong JS solution

--- js/armstrongNumbersSpec.js
+++ js/armstrongNumbersSpec.js
@@ -7,7 +7,7 @@ function createArrayOfNum(maxValue) {
   return Array.apply(null, {length: maxValue}).map(Number.call, Number)
 }
 
-console.log(shallowEqualArrays(arm.findArmstrongNumbers([0]),[0]));
-console.log(shallowEqualArrays(arm.findArmstrongNumbers(createArrayOfNum(8)), [0, 1, 2, 3, 4, 5, 6, 7]));
-console.log(shallowEqualArrays(arm.findArmstrongNumbers(createArrayOfNum(99)), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
-console.log(shallowEqualArrays(arm.findArmstrongNumbers(createArrayOfNum(999)), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407]));
+console.log(shallowEqualArrays(arm.findArmstrongNumbers(0),[0]));
+console.log(shallowEqualArrays(arm.findArmstrongNumbers(8), [0, 1, 2, 3, 4, 5, 6, 7, 8]));
+console.log(shallowEqualArrays(arm.findArmstrongNumbers(99), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
+console.log(shallowEqualArrays(arm.findArmstrongNumbers(999), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407]));
