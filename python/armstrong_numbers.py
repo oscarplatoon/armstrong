af0a97a9394264d9d8d0525c4c055bfbc6353c12
vzbrunstad <vzbrunstad@msn.com>
2021-05-27 21:03:51 -0500

    Vince B armstrong Numbers unittest solution

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,21 @@
-# How can you make this more scalable and reusable later?
+ # How can you make this more scalable and reusable later?
 def find_armstrong_numbers(numbers):
+    armstrong_array = []
+
+    for x in range(len(numbers)):
+        numbers_string = str(x)
+        numbers_list = list(numbers_string)
+        numbers_array = []
+
+        for num in numbers_list:
+            numbers_array.append(int(num))
+
+        sum_of_digits = 0
+
+        for digit in numbers_array:
+            sum_of_digits += digit ** len(numbers_array)
+
+        if x == sum_of_digits:
+            armstrong_array.append(x)
+
+    return armstrong_array
--- python/armstrong_numbers_spec.py
+++ python/armstrong_numbers_spec.py
@@ -1,7 +1,26 @@
 # The code is written as driver code. Can you convert it to Unit Test?
+import unittest
 from armstrong_numbers import find_armstrong_numbers
 
-print(find_armstrong_numbers([0]) == [0]) 
-print(find_armstrong_numbers(list(range(0, 8))) == [0, 1, 2, 3, 4, 5, 6, 7])
-print(find_armstrong_numbers(list(range(0,100))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-print(find_armstrong_numbers(list(range(0,999))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407])
+class armstrong_test(unittest.TestCase):
+    '''Test that the output is a list'''
+    def test_returns_list_type(self):
+        self.assertEqual(type(find_armstrong_numbers([5,6])), list )
+
+    '''test the functions output 0-0'''
+    def test_lists_to_0(self):
+       self.assertEqual(find_armstrong_numbers([0]),[0])
+    '''test the functions output 0-8'''
+    def test_lists_to_8(self):
+       self.assertEqual(find_armstrong_numbers(list(range(0,8))),[0, 1, 2, 3, 4, 5, 6, 7])
+    '''test the functions output 0-100'''
+    def test_lists_to_100(self):
+       self.assertEqual(find_armstrong_numbers(list(range(0,100))),[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    '''test the functions output 0-999'''
+    def test_lists_to_999(self):
+       self.assertEqual(find_armstrong_numbers(list(range(0,999))),[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407])
+    
+
+
+if __name__ == '__main__':
+     unittest.main()
